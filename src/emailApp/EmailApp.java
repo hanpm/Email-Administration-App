3c58c7621e02031fed347c9af767d84c68c2e5f5
Han Pham <41082484+hanpm@users.noreply.github.com>
2024-01-03 11:20:53 -0800

    deleting excess comments

--- src/emailApp/EmailApp.java
+++ src/emailApp/EmailApp.java
@@ -7,14 +7,8 @@ public class EmailApp {
         Email email1 = new Email("Han", "Pham");
 
         email1.setAlternativeEmail("altEmail@gmail.com");
-//        System.out.print("Your alternate email: " + email1.getAlternateEmail());
-
         email1.setMailboxCapacity(100);
-//        System.out.println("\nMailbox capacity: " + email1.getMailboxCapacity());
-
-//        System.out.println("Old password: " + email1.getPassword());
         email1.changePassword("123nnkenkg#$");
-//        System.out.println("New password: " + email1.getPassword());
 
         System.out.print(email1.showInfo());
 
